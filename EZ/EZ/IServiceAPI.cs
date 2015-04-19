7c044bd54bfd806476153a90b9f8bf18adec4cb8
njh08d <njh08d@gmail.com>
2015-04-19 04:37:04 -0400

    added functions

--- EZ/EZ/IServiceAPI.cs
+++ EZ/EZ/IServiceAPI.cs
@@ -7,5 +7,10 @@ namespace EZ
         void CreateNewAccount(string firstName, string lastName, string email, string password, double phonenumber);
         DataTable GetUserDetails(string email);
         bool UserAuthentication(string email, string passsword);
+
+        void SetReservation(string email, string stylist, string service, string sdate, string edate, string year);
+        void CancelReservation(string email);
+        DataTable FindReservations(string email);
+
     }
 }
